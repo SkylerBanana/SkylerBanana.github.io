4a2bf17398c9079f198c03cb526e1e7a863127df
NanaVtuber <vaserial@gmail.com>
2023-08-05 15:13:24 -0400

    Version1.0

---
+++ Logic.js
@@ -0,0 +1,813 @@
+let pokemons = [
+  {
+    "#": "1",
+    "Name": "Bulbasaur",
+    "Type 1": "Grass",
+    "Type 2": "Poison",
+    "HP": "45",
+    "Attack": "49",
+    "Defense": "49",
+    "Sp. Atk": "65",
+    "Sp. Def": "65",
+    "Speed": "45",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "2",
+    "Name": "Ivysaur",
+    "Type 1": "Grass",
+    "Type 2": "Poison",
+    "HP": "60",
+    "Attack": "62",
+    "Defense": "63",
+    "Sp. Atk": "80",
+    "Sp. Def": "80",
+    "Speed": "60",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "3",
+    "Name": "Venusaur",
+    "Type 1": "Grass",
+    "Type 2": "Poison",
+    "HP": "80",
+    "Attack": "82",
+    "Defense": "83",
+    "Sp. Atk": "100",
+    "Sp. Def": "100",
+    "Speed": "80",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "4",
+    "Name": "Charmander",
+    "Type 1": "Fire",
+    "Type 2": "",
+    "HP": "39",
+    "Attack": "52",
+    "Defense": "43",
+    "Sp. Atk": "60",
+    "Sp. Def": "50",
+    "Speed": "65",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "5",
+    "Name": "Charmeleon",
+    "Type 1": "Fire",
+    "Type 2": "",
+    "HP": "58",
+    "Attack": "64",
+    "Defense": "58",
+    "Sp. Atk": "80",
+    "Sp. Def": "65",
+    "Speed": "80",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "6",
+    "Name": "Charizard",
+    "Type 1": "Fire",
+    "Type 2": "Flying",
+    "HP": "78",
+    "Attack": "84",
+    "Defense": "78",
+    "Sp. Atk": "109",
+    "Sp. Def": "85",
+    "Speed": "100",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "7",
+    "Name": "Squirtle",
+    "Type 1": "Water",
+    "Type 2": "",
+    "HP": "44",
+    "Attack": "48",
+    "Defense": "65",
+    "Sp. Atk": "50",
+    "Sp. Def": "64",
+    "Speed": "43",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "8",
+    "Name": "Wartortle",
+    "Type 1": "Water",
+    "Type 2": "",
+    "HP": "59",
+    "Attack": "63",
+    "Defense": "80",
+    "Sp. Atk": "65",
+    "Sp. Def": "80",
+    "Speed": "58",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "9",
+    "Name": "Blastoise",
+    "Type 1": "Water",
+    "Type 2": "",
+    "HP": "79",
+    "Attack": "83",
+    "Defense": "100",
+    "Sp. Atk": "85",
+    "Sp. Def": "105",
+    "Speed": "78",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "10",
+    "Name": "Caterpie",
+    "Type 1": "Bug",
+    "Type 2": "",
+    "HP": "45",
+    "Attack": "30",
+    "Defense": "35",
+    "Sp. Atk": "20",
+    "Sp. Def": "20",
+    "Speed": "45",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "11",
+    "Name": "Metapod",
+    "Type 1": "Bug",
+    "Type 2": "",
+    "HP": "50",
+    "Attack": "20",
+    "Defense": "55",
+    "Sp. Atk": "25",
+    "Sp. Def": "25",
+    "Speed": "30",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "12",
+    "Name": "Butterfree",
+    "Type 1": "Bug",
+    "Type 2": "Flying",
+    "HP": "60",
+    "Attack": "45",
+    "Defense": "50",
+    "Sp. Atk": "90",
+    "Sp. Def": "80",
+    "Speed": "70",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "13",
+    "Name": "Weedle",
+    "Type 1": "Bug",
+    "Type 2": "Poison",
+    "HP": "40",
+    "Attack": "35",
+    "Defense": "30",
+    "Sp. Atk": "20",
+    "Sp. Def": "20",
+    "Speed": "50",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "14",
+    "Name": "Kakuna",
+    "Type 1": "Bug",
+    "Type 2": "Poison",
+    "HP": "45",
+    "Attack": "25",
+    "Defense": "50",
+    "Sp. Atk": "25",
+    "Sp. Def": "25",
+    "Speed": "35",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "15",
+    "Name": "Beedrill",
+    "Type 1": "Bug",
+    "Type 2": "Poison",
+    "HP": "65",
+    "Attack": "90",
+    "Defense": "40",
+    "Sp. Atk": "45",
+    "Sp. Def": "80",
+    "Speed": "75",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "16",
+    "Name": "Pidgey",
+    "Type 1": "Normal",
+    "Type 2": "Flying",
+    "HP": "40",
+    "Attack": "45",
+    "Defense": "40",
+    "Sp. Atk": "35",
+    "Sp. Def": "35",
+    "Speed": "56",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "17",
+    "Name": "Pidgeotto",
+    "Type 1": "Normal",
+    "Type 2": "Flying",
+    "HP": "63",
+    "Attack": "60",
+    "Defense": "55",
+    "Sp. Atk": "50",
+    "Sp. Def": "50",
+    "Speed": "71",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "18",
+    "Name": "Pidgeot",
+    "Type 1": "Normal",
+    "Type 2": "Flying",
+    "HP": "83",
+    "Attack": "80",
+    "Defense": "75",
+    "Sp. Atk": "70",
+    "Sp. Def": "70",
+    "Speed": "101",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "19",
+    "Name": "Rattata",
+    "Type 1": "Normal",
+    "Type 2": "",
+    "HP": "30",
+    "Attack": "56",
+    "Defense": "35",
+    "Sp. Atk": "25",
+    "Sp. Def": "35",
+    "Speed": "72",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "20",
+    "Name": "Raticate",
+    "Type 1": "Normal",
+    "Type 2": "",
+    "HP": "55",
+    "Attack": "81",
+    "Defense": "60",
+    "Sp. Atk": "50",
+    "Sp. Def": "70",
+    "Speed": "97",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "21",
+    "Name": "Spearow",
+    "Type 1": "Normal",
+    "Type 2": "Flying",
+    "HP": "40",
+    "Attack": "60",
+    "Defense": "30",
+    "Sp. Atk": "31",
+    "Sp. Def": "31",
+    "Speed": "70",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "22",
+    "Name": "Fearow",
+    "Type 1": "Normal",
+    "Type 2": "Flying",
+    "HP": "65",
+    "Attack": "90",
+    "Defense": "65",
+    "Sp. Atk": "61",
+    "Sp. Def": "61",
+    "Speed": "100",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "23",
+    "Name": "Ekans",
+    "Type 1": "Poison",
+    "Type 2": "",
+    "HP": "35",
+    "Attack": "60",
+    "Defense": "44",
+    "Sp. Atk": "40",
+    "Sp. Def": "54",
+    "Speed": "55",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "24",
+    "Name": "Arbok",
+    "Type 1": "Poison",
+    "Type 2": "",
+    "HP": "60",
+    "Attack": "85",
+    "Defense": "69",
+    "Sp. Atk": "65",
+    "Sp. Def": "79",
+    "Speed": "80",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "25",
+    "Name": "Pikachu",
+    "Type 1": "Electric",
+    "Type 2": "",
+    "HP": "35",
+    "Attack": "55",
+    "Defense": "40",
+    "Sp. Atk": "50",
+    "Sp. Def": "50",
+    "Speed": "90",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "26",
+    "Name": "Raichu",
+    "Type 1": "Electric",
+    "Type 2": "",
+    "HP": "60",
+    "Attack": "90",
+    "Defense": "55",
+    "Sp. Atk": "90",
+    "Sp. Def": "80",
+    "Speed": "110",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "27",
+    "Name": "Sandshrew",
+    "Type 1": "Ground",
+    "Type 2": "",
+    "HP": "50",
+    "Attack": "75",
+    "Defense": "85",
+    "Sp. Atk": "20",
+    "Sp. Def": "30",
+    "Speed": "40",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "28",
+    "Name": "Sandslash",
+    "Type 1": "Ground",
+    "Type 2": "",
+    "HP": "75",
+    "Attack": "100",
+    "Defense": "110",
+    "Sp. Atk": "45",
+    "Sp. Def": "55",
+    "Speed": "65",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "29",
+    "Name": "Nidoran ♀",
+    "Type 1": "Poison",
+    "Type 2": "",
+    "HP": "55",
+    "Attack": "47",
+    "Defense": "52",
+    "Sp. Atk": "40",
+    "Sp. Def": "40",
+    "Speed": "41",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "30",
+    "Name": "Nidorina",
+    "Type 1": "Poison",
+    "Type 2": "",
+    "HP": "70",
+    "Attack": "62",
+    "Defense": "67",
+    "Sp. Atk": "55",
+    "Sp. Def": "55",
+    "Speed": "56",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "31",
+    "Name": "Nidoqueen",
+    "Type 1": "Poison",
+    "Type 2": "Ground",
+    "HP": "90",
+    "Attack": "92",
+    "Defense": "87",
+    "Sp. Atk": "75",
+    "Sp. Def": "85",
+    "Speed": "76",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "32",
+    "Name": "Nidoran ♂",
+    "Type 1": "Poison",
+    "Type 2": "",
+    "HP": "46",
+    "Attack": "57",
+    "Defense": "40",
+    "Sp. Atk": "40",
+    "Sp. Def": "40",
+    "Speed": "50",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "33",
+    "Name": "Nidorino",
+    "Type 1": "Poison",
+    "Type 2": "",
+    "HP": "61",
+    "Attack": "72",
+    "Defense": "57",
+    "Sp. Atk": "55",
+    "Sp. Def": "55",
+    "Speed": "65",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "34",
+    "Name": "Nidoking",
+    "Type 1": "Poison",
+    "Type 2": "Ground",
+    "HP": "81",
+    "Attack": "102",
+    "Defense": "77",
+    "Sp. Atk": "85",
+    "Sp. Def": "75",
+    "Speed": "85",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "35",
+    "Name": "Clefairy",
+    "Type 1": "Fairy",
+    "Type 2": "",
+    "HP": "70",
+    "Attack": "45",
+    "Defense": "48",
+    "Sp. Atk": "60",
+    "Sp. Def": "65",
+    "Speed": "35",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "36",
+    "Name": "Clefable",
+    "Type 1": "Fairy",
+    "Type 2": "",
+    "HP": "95",
+    "Attack": "70",
+    "Defense": "73",
+    "Sp. Atk": "95",
+    "Sp. Def": "90",
+    "Speed": "60",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "37",
+    "Name": "Vulpix",
+    "Type 1": "Fire",
+    "Type 2": "",
+    "HP": "38",
+    "Attack": "41",
+    "Defense": "40",
+    "Sp. Atk": "50",
+    "Sp. Def": "65",
+    "Speed": "65",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "38",
+    "Name": "Ninetales",
+    "Type 1": "Fire",
+    "Type 2": "",
+    "HP": "73",
+    "Attack": "76",
+    "Defense": "75",
+    "Sp. Atk": "81",
+    "Sp. Def": "100",
+    "Speed": "100",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "39",
+    "Name": "Jigglypuff",
+    "Type 1": "Normal",
+    "Type 2": "Fairy",
+    "HP": "115",
+    "Attack": "45",
+    "Defense": "20",
+    "Sp. Atk": "45",
+    "Sp. Def": "25",
+    "Speed": "20",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "40",
+    "Name": "Wigglytuff",
+    "Type 1": "Normal",
+    "Type 2": "Fairy",
+    "HP": "140",
+    "Attack": "70",
+    "Defense": "45",
+    "Sp. Atk": "85",
+    "Sp. Def": "50",
+    "Speed": "45",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "41",
+    "Name": "Zubat",
+    "Type 1": "Poison",
+    "Type 2": "Flying",
+    "HP": "40",
+    "Attack": "45",
+    "Defense": "35",
+    "Sp. Atk": "30",
+    "Sp. Def": "40",
+    "Speed": "55",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "42",
+    "Name": "Golbat",
+    "Type 1": "Poison",
+    "Type 2": "Flying",
+    "HP": "75",
+    "Attack": "80",
+    "Defense": "70",
+    "Sp. Atk": "65",
+    "Sp. Def": "75",
+    "Speed": "90",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "43",
+    "Name": "Oddish",
+    "Type 1": "Grass",
+    "Type 2": "Poison",
+    "HP": "45",
+    "Attack": "50",
+    "Defense": "55",
+    "Sp. Atk": "75",
+    "Sp. Def": "65",
+    "Speed": "30",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "44",
+    "Name": "Gloom",
+    "Type 1": "Grass",
+    "Type 2": "Poison",
+    "HP": "60",
+    "Attack": "65",
+    "Defense": "70",
+    "Sp. Atk": "85",
+    "Sp. Def": "75",
+    "Speed": "40",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "45",
+    "Name": "Vileplume",
+    "Type 1": "Grass",
+    "Type 2": "Poison",
+    "HP": "75",
+    "Attack": "80",
+    "Defense": "85",
+    "Sp. Atk": "110",
+    "Sp. Def": "90",
+    "Speed": "50",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "46",
+    "Name": "Paras",
+    "Type 1": "Bug",
+    "Type 2": "Grass",
+    "HP": "35",
+    "Attack": "70",
+    "Defense": "55",
+    "Sp. Atk": "45",
+    "Sp. Def": "55",
+    "Speed": "25",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "47",
+    "Name": "Parasect",
+    "Type 1": "Bug",
+    "Type 2": "Grass",
+    "HP": "60",
+    "Attack": "95",
+    "Defense": "80",
+    "Sp. Atk": "60",
+    "Sp. Def": "80",
+    "Speed": "30",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "48",
+    "Name": "Venonat",
+    "Type 1": "Bug",
+    "Type 2": "Poison",
+    "HP": "60",
+    "Attack": "55",
+    "Defense": "50",
+    "Sp. Atk": "40",
+    "Sp. Def": "55",
+    "Speed": "45",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "49",
+    "Name": "Venomoth",
+    "Type 1": "Bug",
+    "Type 2": "Poison",
+    "HP": "70",
+    "Attack": "65",
+    "Defense": "60",
+    "Sp. Atk": "90",
+    "Sp. Def": "75",
+    "Speed": "90",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  },
+  {
+    "#": "50",
+    "Name": "Diglett",
+    "Type 1": "Ground",
+    "Type 2": "",
+    "HP": "10",
+    "Attack": "55",
+    "Defense": "25",
+    "Sp. Atk": "35",
+    "Sp. Def": "45",
+    "Speed": "95",
+    "Generation": "1",
+    "Legendary": "FALSE"
+  }
+];
+let score = 0;
+var answers = [];
+function getRandomPokemon() {
+  const randomPokemon = Math.floor(Math.random() * pokemons.length);
+  return pokemons[randomPokemon].Name;
+}
+
+var randomPokemon1 = getRandomPokemon();
+var randomPokemon2 = getRandomPokemon();
+var randomPokemon3 = getRandomPokemon();
+var currentPokemon = getRandomPokemon();
+
+
+
+function Reset(){
+randomPokemon1 = getRandomPokemon();
+randomPokemon2 = getRandomPokemon();
+randomPokemon3 = getRandomPokemon();
+currentPokemon = getRandomPokemon();
+answers = [];
+answers.push(randomPokemon1,randomPokemon2,randomPokemon3,currentPokemon)
+ answers = shuffle(answers);
+ $("img").attr("src", "Pokemon_imgs/" + currentPokemon + ".png");
+      $("#Answer1").text(answers[0]);
+      $("#Answer2").text(answers[1]);
+      $("#Answer3").text(answers[2]);
+      $("#Answer4").text(answers[3]);
+  
+
+}
+answers.push(randomPokemon1,randomPokemon2,randomPokemon3,currentPokemon)
+console.log(answers)
+ answers=shuffle(answers);
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+$(document).ready(function(){
+  
+      $("img").attr("src", "Pokemon_imgs/" + currentPokemon + ".png");
+      $("#Answer1").text(answers[0]);
+      $("#Answer2").text(answers[1]);
+      $("#Answer3").text(answers[2]);
+      $("#Answer4").text(answers[3]);
+    //future comment
+      $("#Answer1").click(function(){
+        if($(this).text() != currentPokemon){
+          if(score > 0){
+            score -= 1;
+          }
+        }
+        else{
+          score += 1;
+        }
+        $("h2").text("Score:" + score);
+        Reset();
+      });
+      $("#Answer2").click(function(){
+        if($(this).text() != currentPokemon){
+          if(score > 0){
+            score -= 1;
+          }
+        }
+        else{
+          score += 1;
+        }
+        $("h2").text("Score:" + score);
+        Reset();
+      });
+      $("#Answer3").click(function(){
+        if($(this).text() != currentPokemon){
+          if(score > 0){
+            score -= 1;
+          }
+        }
+        else{
+          score += 1;
+        }
+        $("h2").text("Score:" + score);
+        Reset();
+      });
+      $("#Answer4").click(function(){
+        if($(this).text() != currentPokemon){
+          if(score > 0){
+            score -= 1;
+          }
+        }
+        else{
+          score += 1;
+        }
+        $("h2").text("Score:" + score);
+        Reset();
+      });
+});
+
+    console.log(score)
